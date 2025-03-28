b1a004fe96b82bfee0fcd01ebee15f5092e6057a
Farkhad <farhad.begimbetov1@gmail.com>
2025-03-28 11:42:49 +0500

    publish

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -47,13 +47,15 @@ class Dashboard extends Page
         $lineChartMetricData['2024-12-16'] = 8.42;
 
         return [
-            \MoonShine\UI\Components\Layout\Body::make([
+            Grid::make([
+
+                \MoonShine\UI\Components\Layout\Body::make([
                 LineChartMetric::make('Рейтинг')
                     ->line([
                         'Оценка' => $lineChartMetricData
                     ])->columnSpan(10),
             ]),
-
+            ]),
             Grid::make([
                 ValueMetric::make('Сотрудников')
                     ->value(fn() => 27)
